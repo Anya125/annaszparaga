802ca166036189d962655d55925443e090ff62dc
Anna <annaszparaga@gmail.com>
2020-02-06 19:59:32 +0100

    About me + JS update

--- js/main.js
+++ js/main.js
@@ -6,21 +6,9 @@ hamburger.addEventListener("click", function () {
   // Toggle class "is-active"
   hamburger.classList.toggle("is-active");
   menu.classList.toggle("collapsed");
-  // Do something else, like open/close menu
-});
 
-
-// function addBGimg() {
-//   document.getElementById("newHeaderBG").style.backgroundImage = "url('img/gradient.jpg')";
-// }
-
-
-var button = document.querySelector("#newHeaderBG");
-
-button.addEventListener("click", function () {
+  // tutaj kasuje zielone tlo na kompach
   const curImage = document.getElementById("newHeaderBG").style.backgroundImage;
-
-  console.log(curImage);
 
   if (curImage === 'url("img/gradient.jpg")') {
     document.getElementById("newHeaderBG").style.backgroundImage = "";
